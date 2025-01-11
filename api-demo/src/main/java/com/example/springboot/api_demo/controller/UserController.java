e6335e8456367867784557750e9cb2ab25372d49
kaiko0416 <130950966+kaiko0416@users.noreply.github.com>
2025-01-11 15:40:03 +0900

    ユーザー登録api追加 (#2)

--- api-demo/src/main/java/com/example/springboot/api_demo/controller/UserController.java
+++ api-demo/src/main/java/com/example/springboot/api_demo/controller/UserController.java
@@ -6,12 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-// import com.example.springboot.api_demo.dto.UserRequestDto;
+import com.example.springboot.api_demo.dto.UserRequestDto;
 import com.example.springboot.api_demo.dto.UserResponseDto;
 import com.example.springboot.api_demo.usecase.UserUsecase;
 
@@ -34,11 +33,11 @@ public class UserController {
 		return ResponseEntity.ok(user);
 	}
 
-	// @PostMapping
-	// public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto
-	// dto) {
-	// // TODO: ユーザーを作成する
-	// }
+	@PostMapping
+	public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto dto) {
+		final UserResponseDto user = this.usecase.addUser(dto);
+		return ResponseEntity.ok(user);
+	}
 
 	// @PutMapping("/{id}")
 	// public ResponseEntity<UserDto> updateUser(@PathVariable Integer id,
